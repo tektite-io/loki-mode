b350a133d4ada58d10579673ffdcfc87d1a751ce
asklokesh <lokeshmure@live.com>
2026-03-24 03:30:08 -0400

    ui: agent-ae0b12e6 enhancements

--- web-app/dist/assets/activity-DUp0BNI3.js
+++ web-app/dist/assets/activity-DUp0BNI3.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-kPDW4e_b.js";/**
+import{c as t}from"./index-D0VkdcKs.js";/**
  * @license lucide-react v0.577.0 - ISC
  *
  * This source code is licensed under the ISC license.
---
+++ web-app/dist/assets/lock-DNBeAAeB.js
@@ -0,0 +1,11 @@
+import{c as o}from"./index-D0VkdcKs.js";/**
+ * @license lucide-react v0.577.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],t=o("chevron-down",c);/**
+ * @license lucide-react v0.577.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["rect",{width:"18",height:"11",x:"3",y:"11",rx:"2",ry:"2",key:"1w4ew1"}],["path",{d:"M7 11V7a5 5 0 0 1 10 0v4",key:"fwvmzm"}]],r=o("lock",e);export{t as C,r as L};
